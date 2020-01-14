39d705abb74c89e3d897f24af807f4b8aee855a4
Igor Nikitin <nikitin@quorumlegal.ru>
2020-01-14 16:15:36 +0300

    add solution

---
+++ ReactiveFlow/ReactiveFlow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReactiveFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
